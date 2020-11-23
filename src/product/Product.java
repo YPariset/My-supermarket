618c9a24ad94a3d9c0c1566245523a9a29bce69e
YPariset <yoann.pariset@edu.itescia.fr>
2020-11-23 12:13:07 +0100

    Ajout classes CartShopping & Stock

--- src/product/Product.java
+++ src/product/Product.java
@@ -1,9 +1,18 @@
 package product;
 
 public class Product {
+
+    /****************************************************/
+    /***********           Attributes         ***********/
+    /****************************************************/
+
     private String product;
     private int quantity;
     private float price;
+
+    /****************************************************/
+    /***********           Constructor        ***********/
+    /****************************************************/
 
     public Product( String product, int quantity, float price ) {
         this.product = product;
@@ -11,12 +20,29 @@ public class Product {
         this.price = price;
     }
 
+    /****************************************************/
+    /***********            Methods           ***********/
+    /****************************************************/
+
+
+    /****************************************************/
+    /***********            Getters           ***********/
+    /****************************************************/
+
     public String getProduct() { return product; }
-    public void setProduct( String product ) { this.product = product; }
 
     public int getQuantity() { return quantity; }
-    public void setQuantity( int quantity ) { this.quantity = quantity; }
 
     public float getPrice() { return price; }
+
+    /****************************************************/
+    /***********            Setters           ***********/
+    /****************************************************/
+
+
+    public void setProduct( String product ) { this.product = product; }
+
+    public void setQuantity( int quantity ) { this.quantity = quantity; }
+
     public void setPrice( float price ) { this.price = price; }
 }
