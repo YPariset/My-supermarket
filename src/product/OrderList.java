8c2b724cbd44f50135886b72aaa22005362d9785
lea78 <lea.hamzi@edu.itescia.fr>
2020-11-25 17:04:25 +0100

    classe orderList + classe Order = test good

---
+++ src/product/OrderList.java
@@ -0,0 +1,27 @@
+package product;
+
+import java.util.ArrayList;
+
+public class OrderList {
+    private ArrayList<Order> myOrderList = null;
+
+    public OrderList() {
+        myOrderList = new ArrayList<>();
+    }
+
+    public ArrayList<Order> getOrderList() {
+        String result = "";
+        System.out.println("### Order List ###");
+        for (Order myOrder : myOrderList) {
+            result += "Name" + myOrder.getUserLambda() + " | Shopping List : " + myOrder.getShoppingCart() +
+                    " | Total Amount: " + myOrder.getTotalAmount() + " | ID Order" + myOrder.getOrderID()+ "\n";
+        }
+        System.out.println(result);
+        return myOrderList;
+    }
+
+    public void addOrderToList( Order myOrder){
+        myOrderList.add(myOrder);
+    }
+
+}
--- src/product/test.java
+++ src/product/test.java
@@ -1,24 +1,29 @@
 package product;
 
-import product.StockProduct;
-import user.User;
 
+import user.User;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 
 public class test {
-    public static ArrayList<Order> orderList = new ArrayList<Order>();
+
 
 
     public static void main(String[] args) {
-        Order order = new Order();
-        Scanner sc = new Scanner(System.in);
+        OrderList myOrderList = new OrderList();
+
         CartShopping myshoppingList = new CartShopping();
         User guest = new User("lea","lea");
+
         Order test1 = new Order(guest ,myshoppingList,30.f,1);
-        orderList.add(test1);
-        System.out.println(orderList.contains(test1));
+        Order test2 = new Order(guest,myshoppingList,50.f,2);
+
+        myOrderList.addOrderToList(test1);
+        myOrderList.addOrderToList(test2);
+
+        myOrderList.getOrderList();
+
 
 
 
